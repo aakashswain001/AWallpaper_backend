8d03c638dceb407c613282d98688cf0d169a5c33
Akash Swain <aakashswain001@gmail.com>
2018-05-15 15:38:44 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AWallpaper_backend
---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <link href="css/dropzone.css" type="text/css" rel="stylesheet"/>
+    <script src="js/dropzone.js"></script>
+</head>
+
+<body>
+
+<form action="upload.php" class="dropzone"></form>
+
+</body>
+
+</html>
